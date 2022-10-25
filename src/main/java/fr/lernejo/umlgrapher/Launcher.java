92c812d35193dc03861da5d220b0108b9770e55a
Carineee <chen.carine@hotmail.fr>
2022-10-26 01:49:50 +0200

    Utilisation du prog en ligne de commande (Partie2)

---
+++ src/main/java/fr/lernejo/umlgrapher/Launcher.java
@@ -0,0 +1,29 @@
+package fr.lernejo.umlgrapher;
+
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+import java.util.concurrent.Callable;
+
+@Command(name = "Launcher", mixinStandardHelpOptions = true, version = "uml graph 1.0", description = "uml graph")
+public class Launcher implements Callable<Integer> {
+
+    @Option(names = {"-c", "--classes"}, required = true, description = "Select the class to analyse")
+    private final Class<?>[] CName;
+
+    @Option(names = {"-g", "--graph-type"}, defaultValue = "Mermaid", description = "Select type of graph that you want in the output")
+    private final String graphType = GraphType.Mermaid;
+
+    @Override
+    public Integer call() throws Exception {
+        UmlGraph graph = new UmlGraph(CName);
+        System.out.println(graph.as(GraphType.valueOf(graphType)));
+        return null;
+    }
+
+    public static void main(String... args) {
+         int exitCode = new CommandLine(new Launcher()).execute(args);
+            System.exit(exitCode);
+    }
+}
--- src/main/java/fr/lernejo/umlgrapher/UmlGraph.java
+++ src/main/java/fr/lernejo/umlgrapher/UmlGraph.java
@@ -2,17 +2,17 @@ package fr.lernejo.umlgrapher;
 
 public class UmlGraph {
 
-    private final Class[] Cname;
+    private final Class<?>[] Cname;
 
-    public UmlGraph(Class[] Cname){
+    public UmlGraph(Class<?>[] Cname){
         this.Cname = Cname;
     }
 
-    public String as (GraphType dimension){
+    public String as (GraphType graphType){
         String answer = "classDiagram";
 
-        for (Class myClass : Cname){
-            if (dimension == GraphType.Mermaid) {
+        for (Class<?> myClass : Cname){
+            if (graphType == GraphType.Mermaid) {
                 answer +="class" + myClass.getSimpleName() + "{\n";
                 if (myClass.isInterface()){
                     answer += "<<interface\n";
