18fffea33f63aa53c579faf99d592267b9f8528d
Carineee <chen.carine@hotmail.fr>
2022-10-26 00:07:09 +0200

    Premier test (Partie 1)

--- src/main/java/fr/lernejo/umlgrapher/UmlGraph.java
+++ src/main/java/fr/lernejo/umlgrapher/UmlGraph.java
@@ -1,10 +1,24 @@
 package fr.lernejo.umlgrapher;
 
 public class UmlGraph {
-    private final Class<?>[] classes;
 
-    public UmlGraph(Class<?>[] classes){
-        this.classes = classes;
+    private final Class<?>[] Cname;
+
+    public UmlGraph(Class<?>[] Cname){
+        this.Cname = Cname;
     }
-    
+
+    public String as (GraphType dimension){
+        String answer = "classDiagram";
+
+        for (Class myClass : Cname){
+            if (dimension == GraphType.Mermaid) {
+                answer +="class" + myClass.getSimpleName() + "{\n";
+                if (myClass.isInterface()){
+                    answer += "<<interface\n";
+                }
+                answer += "}\n";
+            }
+        }
+        return answer;}
 }
