4c75c9901636ad32562da512b87c66b78e101557
Carineee <chen.carine@hotmail.fr>
2022-10-27 16:30:53 +0200

    Types parents (Partie3)

--- src/main/java/fr/lernejo/umlgrapher/InternalGraphRepresentation.java
+++ src/main/java/fr/lernejo/umlgrapher/InternalGraphRepresentation.java
@@ -4,23 +4,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class InternalGraphRepresentation {
-    private final Class myclass;
-    InternalGraphRepresentation(Class myclass){
-        this.myclass = myclass;
+
+    private final Class classInterGraph;
+    InternalGraphRepresentation(Class classInterGraph){
+        this.classInterGraph = classInterGraph;
     }
 
     public List<Class> recursive_relation(List<Class> recursiveList,Class... otherClasses){
-        for(Class i : otherClasses){
-            if(!recursiveList.contains(i)) {
-                recursiveList.add(i);
-                recursiveList = recursive_relation(recursiveList,i.getInterfaces());
+        for(Class k : otherClasses){
+            if(!recursiveList.contains(k)) {
+                recursiveList.add(k);
+                recursiveList = recursive_relation(recursiveList,k.getInterfaces());
             }
         }
         return recursiveList;
     }
     public List<Class> whats_your_relation(){
-        List<Class> myList = new ArrayList<Class>();
-        myList = recursive_relation(myList,this.myclass);
-        return myList;
+        List<Class> List = new ArrayList<Class>();
+        List = recursive_relation(List,this.classInterGraph);
+        return List;
     }
 }
